f94ab3d6431369bd6abbd60e5f0719324192234a
JamalyYao <yaokangjun@foxmail.com>
2014-09-30 16:04:03 +0800

    Update BlockingQueueDemo.java

--- src/com/yao/thread/syn/BlockingQueueDemo.java
+++ src/com/yao/thread/syn/BlockingQueueDemo.java
@@ -6,7 +6,7 @@ import java.util.concurrent.BlockingQueue;
 public class BlockingQueueDemo {
 
 	public static void main(String[] args) {
-		BlockingQueue<Integer> bq = new ArrayBlockingQueue<Integer>(1);//ÈÝÁ¿Îª1
+		BlockingQueue<Integer> bq = new ArrayBlockingQueue<Integer>(1);
 		new Thread(new BlockingQueueConsumer(bq)).start();
 		new Thread(new BlockingQueueProducter(bq)).start();
 	}
